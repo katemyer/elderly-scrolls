8f59e9b3629b5671a974d6a274a2cd9fcdb66aa9
kate.m <kate.mangubat@highspot.com>
2021-08-02 00:21:02 -0700

    show image, set up div to do css

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -8,13 +8,26 @@ class Card extends React.Component {
     super(props);
   }
   render() {
-    return <div> name : {this.props.name} rarity : {this.props.rarity} text : {this.props.text} imageUrl : {this.props.imageUrl} setName : {this.props.setName}</div>;
+    return (
+      <div> 
+        <div><img src={this.props.imageUrl}></img></div>
+        <div>name : {this.props.name} </div>
+        <div>rarity : {this.props.rarity} </div>
+        <div>text : {this.props.text} </div>
+        <div>setName : {this.props.setName}</div>
+      </div>)
   }
 }
 
+//at minimum, card should have a name
 Card.propTypes = {
   name: PropTypes.string
 }
 
 export default Card;
 
+//to do
+  //format html correctly with proper divs
+  //remember to use flex box
+  //apply css to the divs
+  //get the actual image, use img tag
