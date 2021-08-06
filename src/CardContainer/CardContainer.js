d0a03c7b8b5478783ee0901dc4152b32cb1ef25c
kate.m <kate.mangubat@highspot.com>
2021-08-05 18:51:29 -0700

    infinite scroll functionality

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -12,30 +12,43 @@ class CardContainer extends React.Component {
           error: null,
           isLoaded: true,
           cards: [],
-          page: 1,
+          hasMore: true,
         };
+        this.userSearchValue = null;
+        this.page = 1;
+        this.loadingMessage = "";
         this.handleOnClick = this.handleOnClick.bind(this);
         this.getCards =  this.getCards.bind(this);
+        this.myFunction = this.myFunction.bind(this);
+        this.cards = [];
       }
       
     componentDidMount() {
         console.log("component did mount")
-        this.getCards();
+        // this.getCards();
     }
 
     getCards() {
-        const PAGE_SIZE = 20;
-        let page = this.state.page;
-        const cards = this.state.cards;
+        const PAGE_SIZE = 20 ;
+        let page = this.page;
+        const cards = this.cards;
         console.log('what is the infinite scroll library doing? ', page);
-        axios.get(`https://api.elderscrollslegends.io/v1/cards?pageSize=${PAGE_SIZE}&page=${page}`)
+        axios.get(`https://api.elderscrollslegends.io/v1/cards?pageSize=${PAGE_SIZE}&page=${page}&name=${this.userSearchValue}`)
             .then((response)=> {
             // console.log("hi" +JSON.stringify(response));
             const cardResults = response.data.cards;
-            console.log('card results', cardResults);
-        
-            this.setState({cards: cards.concat(cardResults), page: page++}); // todo: update the page #
-            console.log("this.setState", this.setState);
+            // console.log('card results', cardResults);
+            //logic for when hasMore becomes false to stop scroll
+            //if cardResults == page_size, scroll
+            if(cardResults.length < PAGE_SIZE ) {
+                // block of code to be executed if the condition is true
+                this.setState({hasMore: false}); 
+              }
+            //else cardResults < page_size, stop scroll
+            this.setState({cards: cards.concat(cardResults)});
+            this.cards = cards.concat(cardResults);
+            this.page = page + 1; // todo: update the page #
+            // console.log("this.setState", this.setState);
             }).catch(function (error) {
                 if(error.response){
                     alert(error.response.status + ":" +error.response.statusText + ":"+ error.response.data )
@@ -43,40 +56,49 @@ class CardContainer extends React.Component {
                 console.log("Error" + error);
             })  
     }
-    handleOnClick() {
-        console.log("Hello World");
-        let page = this.state.page;
-        // TODO add params to search function, too!
-        const PAGE_SIZE = 20;
+
+    myFunction() {
+        this.cards = [];
         const userSearchValue = document.getElementById('searchBarTextField').value.toLowerCase();
+        // this.setState({userSearchValue: userSearchValue});
+        // this.setState((state, props) => ({
+        //     userSearchValue: userSearchValue
+        //   }));
+        this.userSearchValue = userSearchValue;
         console.log("User Search Value:", userSearchValue);
-        //call this API: https://api.elderscrollslegends.io/v1/cards&name=dragon (for example)
-        // TODO inspect parameters and make sure name, pageSize, page
-        axios.get(`https://api.elderscrollslegends.io/v1/cards?pageSize=${PAGE_SIZE}&page=${page}&name=${userSearchValue}`)
-            .then((response)=> {
-            console.log("hi" +JSON.stringify(response));
-            const cardResults = response.data.cards;
-            const matchedCards = cardResults.filter((card) => { 
-                let str = card.name.toLowerCase();
-                return str.search(userSearchValue) !== -1 }
-                );
-            console.log("match cards:", matchedCards);
-            //https://reactjs.org/docs/state-and-lifecycle.html
-            this.setState({cards: matchedCards});
-            console.log("this.setState", this.setState);
-            }).catch(function (error) {
-                if(error.response){
-                    alert(error.response.status + ":" +error.response.statusText + ":"+ error.response.data )
-                }
-                console.log("Error" + error);
-            })  
+        // axios.get(`https://api.elderscrollslegends.io/v1/cards?pageSize=${PAGE_SIZE}&page=${page}&name=${userSearchValue}`)
+        //     .then((response)=> {
+        //     console.log("hi" +JSON.stringify(response));
+        //     const cardResults = response.data.cards;
+        //     const matchedCards = cardResults.filter((card) => { 
+        //         let str = card.name.toLowerCase();
+        //         return str.search(userSearchValue) !== -1 }
+        //         );
+        //     console.log("match cards:", matchedCards);
+        //     //https://reactjs.org/docs/state-and-lifecycle.html
+        //     this.setState({cards: matchedCards});
+        //     console.log("this.setState", this.setState);
+        //     }).catch(function (error) {
+        //         if(error.response){
+        //             alert(error.response.status + ":" +error.response.statusText + ":"+ error.response.data )
+        //         }
+        //         console.log("Error" + error);
+            // })  
+        this.loadingMessage = "Hold on, getting more cards...";
+        this.getCards();
+    }
+
+    handleOnClick() {
+        //clear out search results before interacting with API: reset page + reset cards
+        this.page = 1;
+        this.setState({cards: [], hasMore: true}, this.myFunction);
     }
   render() {
     const { error, isLoaded, cards } = this.state;
       if (error) {
         return <div>Error: {error.message}</div>;
       } else if (!isLoaded) {
-        return <div>Loading...</div>;
+        return <div>IS THIS ONE LOADING?...</div>;
       } else {
         return (
             <div>
@@ -93,8 +115,13 @@ class CardContainer extends React.Component {
                 <InfiniteScroll
                     dataLength={cards.length}
                     next={this.getCards}
-                    hasMore={true}
-                    loader={<h4>Loading...</h4>}
+                    hasMore={this.state.hasMore}
+                    loader={this.loadingMessage}
+                    endMessage={
+                        <p style={{ textAlign: 'center' }}>
+                            <b>Yay! You have seen it all</b>
+                        </p>
+                        }
                 >
                     {cards.map(card => (
                         <Card
